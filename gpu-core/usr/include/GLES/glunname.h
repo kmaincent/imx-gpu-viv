cc9f92dc0a6d828040415210a3ac468a8407f173
Kory Maincent <kory.maincent@bootlin.com>
2020-05-04 15:47:22 +0200

    Add all to source
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

---
+++ gpu-core/usr/include/GLES/glunname.h
@@ -0,0 +1,299 @@
+/****************************************************************************
+*
+*    Copyright 2012 - 2018 Vivante Corporation, Santa Clara, California.
+*    All Rights Reserved.
+*
+*    Permission is hereby granted, free of charge, to any person obtaining
+*    a copy of this software and associated documentation files (the
+*    'Software'), to deal in the Software without restriction, including
+*    without limitation the rights to use, copy, modify, merge, publish,
+*    distribute, sub license, and/or sell copies of the Software, and to
+*    permit persons to whom the Software is furnished to do so, subject
+*    to the following conditions:
+*
+*    The above copyright notice and this permission notice (including the
+*    next paragraph) shall be included in all copies or substantial
+*    portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+*    IN NO EVENT SHALL VIVANTE AND/OR ITS SUPPLIERS BE LIABLE FOR ANY
+*    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+*    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+*    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+
+
+#ifndef __glunname_h_
+#define __glunname_h_
+
+#if defined(_GL_11_APPENDIX)
+
+#undef glAlphaFunc
+#undef glActiveTexture
+#undef glAlphaFuncx
+#undef glAlphaFuncxOES
+#undef glBindBuffer
+#undef glBindFramebufferOES
+#undef glBindRenderbufferOES
+#undef glBindTexture
+#undef glBlendFunc
+#undef glBufferData
+#undef glBufferSubData
+#undef glCheckFramebufferStatusOES
+#undef glClear
+#undef glClearColor
+#undef glClearColorx
+#undef glClearColorxOES
+#undef glClearDepthf
+#undef glClearDepthfOES
+#undef glClearDepthx
+#undef glClearDepthxOES
+#undef glClearStencil
+#undef glClientActiveTexture
+#undef glClipPlanef
+#undef glClipPlanefOES
+#undef glClipPlanex
+#undef glClipPlanexOES
+#undef glColor4f
+#undef glColor4ub
+#undef glColor4x
+#undef glColor4xOES
+#undef glColorMask
+#undef glColorPointer
+#undef glCompressedTexImage2D
+#undef glCompressedTexSubImage2D
+#undef glCopyTexImage2D
+#undef glCopyTexSubImage2D
+#undef glCullFace
+#undef glCurrentPaletteMatrixOES
+#undef glDeleteBuffers
+#undef glDeleteFramebuffersOE
+#undef glDeleteRenderbuffersOES
+#undef glDeleteTextures
+#undef glDepthFunc
+#undef glDepthMask
+#undef glDepthRangef
+#undef glDepthRangefOES
+#undef glDepthRangex
+#undef glDepthRangexOES
+#undef glDisable
+#undef glDisableClientState
+#undef glDrawArrays
+#undef glDrawElements
+#undef glDrawTexfOES
+#undef glDrawTexfvOES
+#undef glDrawTexiOES
+#undef glDrawTexivOES
+#undef glDrawTexsOES
+#undef glDrawTexsvOES
+#undef glDrawTexxOES
+#undef glDrawTexxvOES
+#undef glEGLImageTargetRenderbufferStorageOES
+#undef glEGLImageTargetTexture2DOES
+#undef glEnable
+#undef glEnableClientState
+#undef glFinish
+#undef glFlush
+#undef glFogf
+#undef glFogfv
+#undef glFogx
+#undef glFogxOES
+#undef glFogxv
+#undef glFogxvOES
+#undef glFramebufferRenderbufferOES
+#undef glFramebufferTexture2DOES
+#undef glFrontFace
+#undef glFrustumf
+#undef glFrustumfOES
+#undef glFrustumx
+#undef glFrustumxOES
+#undef glGenBuffers
+#undef glGenerateMipmapOES
+#undef glGenFramebuffersOES
+#undef glGenRenderbuffersOES
+#undef glGenTextures
+#undef glGetBooleanv
+#undef glGetBufferParameteriv
+#undef glGetClipPlanef
+#undef glGetClipPlanefOES
+#undef glGetClipPlanex
+#undef glGetClipPlanexOES
+#undef glGetError
+#undef glGetFixedv
+#undef glGetFixedvOES
+#undef glGetFloatv
+#undef glGetFramebufferAttachmentParameterivOES
+#undef glGetIntegerv
+#undef glGetLightfv
+#undef glGetLightxv
+#undef glGetLightxvOES
+#undef glGetMaterialfv
+#undef glGetMaterialxv
+#undef glGetMaterialxvOES
+#undef glGetPointerv
+#undef glGetRenderbufferParameterivOES
+#undef glGetString
+#undef glGetTexEnvfv
+#undef glGetTexEnviv
+#undef glGetTexEnvxv
+#undef glGetTexEnvxvOES
+#undef glGetTexGenfvOES
+#undef glGetTexGenivOES
+#undef glGetTexGenxvOES
+#undef glGetTexParameterfv
+#undef glGetTexParameteriv
+#undef glGetTexParameterxv
+#undef glGetTexParameterxvOES
+#undef glHint
+#undef glIsBuffer
+#undef glIsEnabled
+#undef glIsFramebufferOES
+#undef glIsRenderbufferOES
+#undef glIsTexture
+#undef glLightModelf
+#undef glLightModelfv
+#undef glLightModelx
+#undef glLightModelxOES
+#undef glLightModelxv
+#undef glLightModelxvOES
+#undef glLightf
+#undef glLightfv
+#undef glLightx
+#undef glLightxOES
+#undef glLightxv
+#undef glLightxvOES
+#undef glLineWidth
+#undef glLineWidthx
+#undef glLineWidthxOES
+#undef glLoadIdentity
+#undef glLoadMatrixf
+#undef glLoadMatrixx
+#undef glLoadMatrixxOES
+#undef glLoadPaletteFromModelViewMatrixOES
+#undef glLogicOp
+#undef glMaterialf
+#undef glMaterialfv
+#undef glMaterialx
+#undef glMaterialxOES
+#undef glMaterialxv
+#undef glMaterialxvOES
+#undef glMatrixIndexPointerOES
+#undef glMatrixMode
+#undef glMultMatrixf
+#undef glMultMatrixx
+#undef glMultMatrixxOES
+#undef glMultiTexCoord4f
+#undef glMultiTexCoord4x
+#undef glMultiTexCoord4xOES
+#undef glNormal3f
+#undef glNormal3x
+#undef glNormal3xOES
+#undef glNormalPointer
+#undef glOrthof
+#undef glOrthofOES
+#undef glOrthox
+#undef glOrthoxOES
+#undef glPixelStorei
+#undef glPointParameterf
+#undef glPointParameterfv
+#undef glPointParameterx
+#undef glPointParameterxOES
+#undef glPointParameterxv
+#undef glPointParameterxvOES
+#undef glPointSize
+#undef glPointSizePointerOES
+#undef glPointSizex
+#undef glPointSizexOES
+#undef glPolygonOffset
+#undef glPolygonOffsetx
+#undef glPolygonOffsetxOES
+#undef glPopMatrix
+#undef glPushMatrix
+#undef glQueryMatrixxOES
+#undef glReadPixels
+#undef glRenderbufferStorageOES
+#undef glRotatef
+#undef glRotatex
+#undef glRotatexOES
+#undef glSampleCoverage
+#undef glSampleCoveragex
+#undef glSampleCoveragexOES
+#undef glScalef
+#undef glScalex
+#undef glScalexOES
+#undef glScissor
+#undef glShadeModel
+#undef glStencilFunc
+#undef glStencilMask
+#undef glStencilOp
+#undef glTexCoordPointer
+#undef glTexEnvf
+#undef glTexEnvfv
+#undef glTexEnvi
+#undef glTexEnviv
+#undef glTexEnvx
+#undef glTexEnvxOES
+#undef glTexEnvxv
+#undef glTexEnvxvOES
+#undef glTexGenfOES
+#undef glTexGenfvOES
+#undef glTexGeniOES
+#undef glTexGenivOES
+#undef glTexGenxOES
+#undef glTexGenxvOES
+#undef glTexImage2D
+#undef glTexParameterf
+#undef glTexParameterfv
+#undef glTexParameteri
+#undef glTexParameteriv
+#undef glTexParameterx
+#undef glTexParameterxOES
+#undef glTexParameterxv
+#undef glTexParameterxvOES
+#undef glTexSubImage2D
+#undef glTranslatef
+#undef glTranslatex
+#undef glTranslatexOES
+#undef glVertexPointer
+#undef glViewport
+#undef glWeightPointerOES
+
+#undef glMultiDrawArraysEXT
+#undef glMultiDrawElementsEXT
+
+#undef glMapBufferOES
+#undef glUnmapBufferOES
+#undef glGetBufferPointervOES
+
+#undef glBlendEquationOES
+#undef glBlendEquationSeparateOES
+#undef glBlendFuncSeparateOES
+
+#undef glIsFramebufferOES
+#undef glBindFramebufferOES
+#undef glDeleteFramebuffersOES
+#undef glGenFramebuffersOES
+#undef glCheckFramebufferStatusOES
+#undef glFramebufferRenderbufferOES
+#undef glFramebufferTexture2DOES
+
+#undef glClipPlanefIMG
+#undef glClipPlanexIMG
+
+#undef glTexDirectInvalidateVIV
+#undef glTexDirectVIV
+#undef glTexDirectVIVMap
+#undef glTexDirectTiledMapVIV
+
+#undef glBindBufferARB
+#undef glBufferDataARB
+#undef glBufferSubDataARB
+#undef glDeleteBuffersARB
+#undef glGenBuffersARB
+#undef glGetBufferParameterivARB
+
+#endif /* _GL_11_APPENDIX */
+#endif /* __glunname_h_ */
